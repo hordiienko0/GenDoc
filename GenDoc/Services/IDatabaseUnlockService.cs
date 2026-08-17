72d3f6d2f0ddcf4861cf531d74c617fac5e63ff4
hordiienko0 <kolyahordiienko19@gmail.com>
2026-08-17 10:31:37 +0300

    Прибрано невживані using в IDatabaseUnlockService
    Правка не з цієї роботи — прилетіла від автоочищення Visual Studio під час
    сеансу відлагодження. Комітиться окремо, щоб не змішуватися зі змінами по суті.

--- GenDoc/Services/IDatabaseUnlockService.cs
+++ GenDoc/Services/IDatabaseUnlockService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GenDoc.Services
+﻿namespace GenDoc.Services
 {
     public interface IDatabaseUnlockService
     {
